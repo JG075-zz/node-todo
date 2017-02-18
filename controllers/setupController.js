e224579ad260f335a90c7fe0b100541b590ff730
James Gardiner <jamesgardiner075@gmail.com>
2017-02-18 16:51:04 +0000

    Add setup controller for seed data

---
+++ controllers/setupController.js
@@ -0,0 +1,32 @@
+var Todos = require('../models/todoModel.js');
+
+module.exports = function(app) {
+
+  app.get('/api/setupTodos', function(req, res){
+
+    // seed database
+    var starterTodos = [
+      {
+        username: 'test',
+        todo: 'Buy milk',
+        idDone: false,
+        hasAttachment: false
+      },
+      {
+        username: 'test',
+        todo: 'Feed dog',
+        idDone: false,
+        hasAttachment: false
+      },
+      {
+        username: 'test',
+        todo: 'Learn Node',
+        idDone: false,
+        hasAttachment: false
+      }
+    ];
+    Todos.create(starterTodos, function(err, results) {
+      res.send(results);
+    });
+  });
+};
